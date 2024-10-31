1a1d73cb3c4b0a6a679d84a676d9b0838cf9fd71
faisal795 <buntijani795@gmail.com>
2024-10-31 20:42:20 +0500

    Bug Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import Flickity from './Components/Flickity/Flickity';
-import Header from './Components/Header/Header';
+import React from "react";
+import Flickity from "./Components/Flickity/Flickity";
+import Header from "./Components/Header/Header";
 
 const App = () => {
   return (
     <>
-    
-    <Flickity></Flickity>
-    <Header></Header>
-  
-  </>
-  )
-}
+      <Flickity></Flickity>
+      <Header></Header>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,33 +4,49 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useState } from "react";
 
-const NavLinks = ({OpenNav}) => {
+const NavLinks = ({ OpenNav }) => {
   console.log(OpenNav, "NavLinks");
-  
+
   return (
     <>
-    {/* hidden lg:flex */}
-      <ul className={`transition-all duration-300 ease-in-out ${OpenNav? "left-0" : "left-[-332px]"} absolute  w-[320px] h-full z-[1000] bg-white lg:relative lg:left-auto lg:w-auto lg:h-auto lg:z-auto lg:bg-transparent lg:flex`} >
+      {/* hidden lg:flex */}
+      <ul
+        className={`transition-all duration-300 ease-in-out ${
+          OpenNav ? "left-0" : "left-[-332px]"
+        } absolute  w-[320px] h-full z-[1000] bg-white lg:relative lg:left-auto lg:w-auto lg:h-auto lg:z-auto lg:bg-transparent lg:flex`}
+      >
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Home</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Home
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Evoting</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Evoting
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Donation</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Donation
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Events</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Events
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Results</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Results
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">Blog</li>
+          <li className="w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
+            Blog
+          </li>
         </Link>
         <Link className="px-[18px] py-0 flex items-center justify-center hover:text-[#01455d] cursor-pointer">
-          <li className="flex items-center justify-between w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0" >
+          <li className="flex items-center justify-between w-full py-[12px] pr-[15px] pl-[26px] lg:w-auto lg:py-0 lg:pr-0 lg:pl-0">
             Pages <RiArrowDropDownLine className="text-[22px]" />
           </li>
         </Link>
@@ -49,12 +65,11 @@ const Header = () => {
   const toggleNavFunction = () => {
     setisOpenNav(!isOpenNav);
     console.log(isOpenNav);
-    
   };
 
   return (
     <>
-    {/* jkh  */}
+      {/* jkh  */}
       <div className="w-full h-[80px] bg-white text-[14px] font-medium transition-all duration-500 flex items-center justify-center">
         <div className="container flex justify-between lg:justify-between mx-10 px-10 ">
           <div className="right">
@@ -65,7 +80,10 @@ const Header = () => {
           <div className="left">
             <NavLinks OpenNav={isOpenNav} />
             {/* className={`hamburger ${isOpenNav? "open" : ""}`} onClick={toggleNavFunction} */}
-            <div  className="flex justify-center items-center  lg:hidden"  onClick={toggleNavFunction}>
+            <div
+              className="flex justify-center items-center  lg:hidden"
+              onClick={toggleNavFunction}
+            >
               <RxHamburgerMenu />
             </div>
           </div>
